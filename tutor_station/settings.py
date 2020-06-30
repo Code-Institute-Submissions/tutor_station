a0b845b47303743e133c0f1e0577840a63c3bfa9
John Hammer <johnwhammer@gmail.com>
2020-06-30 22:03:18 +0000

    damn it

--- tutor_station/settings.py
+++ tutor_station/settings.py
@@ -115,12 +115,6 @@ WSGI_APPLICATION = 'tutor_station.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-
-else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
